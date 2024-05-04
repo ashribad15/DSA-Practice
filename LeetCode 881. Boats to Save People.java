fde0f8e81fbb9143aa65b3abd3cbc1bde655325a
ASHRIBAD PANDA <133385084+ashribad15@users.noreply.github.com>
2024-05-04 13:31:28 +0530

    Create LeetCode 881. Boats to Save People.java

---
+++ LeetCode 881. Boats to Save People.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int numRescueBoats(int[] people, int limit)
+     {
+        Arrays.sort(people);
+        
+        int i=0,j=people.length-1;
+        int c=0;
+
+        while(i<=j)
+        {
+                 if(people[i]+people[j]<=limit)
+                 {
+                    c++;
+                    i++;
+                    j--;
+                 }
+                 else
+                 {
+                    j--;
+                    c++;
+                 }
+        }
+
+        return c;
+        
+    }
+}
