3ecba28623342a1f816115ad3b9a2ab2eca53c45
ASHRIBAD PANDA <133385084+ashribad15@users.noreply.github.com>
2024-05-26 20:40:55 +0530

    Create LeetCode 552. Student Attendance Record II.java

---
+++ LeetCode 552. Student Attendance Record II.java
@@ -0,0 +1,30 @@
+class Solution 
+{
+    private final int MOD = 1000000007;
+    public int checkRecord(int n)
+     {
+        int[][][] memo = new int[n+1][3][2];
+        for(int[][] d: memo){
+            for(int[] row: d) Arrays.fill(row, -1);
+        }
+        return solve(n, 0, 0, memo);
+    }
+
+    private int solve(int n, int consecutiveLates, int absents, int[][][] memo)
+    {
+        if(absents >= 2 || consecutiveLates >= 3) return 0;
+        if(n == 0) return 1;
+
+
+        if(memo[n][consecutiveLates][absents] != -1) return memo[n][consecutiveLates][absents];
+        int ans = 0;
+        ans = (solve(n-1, 0, absents+1, memo)) % MOD;
+
+        ans = (ans + solve(n-1, consecutiveLates+1, absents, memo)) % MOD;
+
+        ans = (ans + solve(n-1, 0, absents, memo)) % MOD;
+
+        return memo[n][consecutiveLates][absents] = ans;
+        
+    }
+}
