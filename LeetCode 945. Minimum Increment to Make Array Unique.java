8bc6bf1d66bfd3e30d5db4317c1fa3499dba3d15
ASHRIBAD PANDA <133385084+ashribad15@users.noreply.github.com>
2024-06-14 22:21:12 +0530

    Create LeetCode 945. Minimum Increment to Make Array Unique.java

---
+++ LeetCode 945. Minimum Increment to Make Array Unique.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int minIncrementForUnique(int[] nums)
+     {
+        Arrays.sort(nums);
+        int s=0;
+        
+        for(int i=1;i<nums.length;i++)
+        {
+            if(nums[i]<=nums[i-1])
+            {
+                     s=s+(nums[i-1]-nums[i])+1;
+                     nums[i]=nums[i-1]+1;
+
+            }
+        }
+
+        return s;
+        
+    }
+}
