bed41b4b844978630c928fe7e46a3c1685bb9f28
ASHRIBAD PANDA <133385084+ashribad15@users.noreply.github.com>
2024-06-22 22:40:12 +0530

    Update LeetCode 1248. Count Number of Nice Subarrays.java

--- LeetCode 1248. Count Number of Nice Subarrays.java
+++ LeetCode 1248. Count Number of Nice Subarrays.java
@@ -1,1 +1,24 @@
+class Solution 
+{
+    public int numberOfSubarrays(int[] nums, int k) 
+    {
+      HashMap<Integer, Integer> map = new HashMap<>();
+      int c=0,s=0;
+
+      for(int i=0;i<nums.length;i++)
+        {
+          if(nums[i]%2==1)
+            c++;
+          
+         if( map.contains(c-k))
+         {
+           s=s+map.get(c-k);
+         }
+
+          map.put(c,map.getOrDefault(c,0)+1);
+        }
+
+      return s;
+    }
+}
 
